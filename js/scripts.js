bed8b38cca17fb4291360ffe8b728011412bb271
nsncowan <nsncowan@gmail.com>
2022-08-17 20:07:08 -0700

    add additional arithmetic to js code.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,25 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 // user interface logic
 const number1 = parseInt(prompt("enter a number"));
 const number2 = parseInt(prompt("enter another number"));
 
-window.alert(add(number1,number2));
+window.alert(
+  number1.toString() + " + " + number2.toString() + " = " + add(number1,number2)+ "\n" +
+  number1.toString() + " - " + number2.toString() + " = " + subtract(number1,number2)+ "\n" +
+  number1.toString() + " * " + number2.toString() + " = " + multiply(number1,number2) + "\n" +
+  number1.toString() + " / " + number2.toString() + " = " + divide(number1,number2));
 
